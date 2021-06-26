d2d0c355f405438a9e460436dd5e461feea8ab2f
kkreddypokuru <kkreddypokuru@gmail.com>
2021-06-27 04:37:20 +0530

    search linear algo

--- LinearSearch/search2.py
+++ LinearSearch/search2.py
@@ -1,32 +1,40 @@
-# Time complexity O(n)
-import logging
-import timeit
+# Python3 program for linear search
+def search(arrayBucket, search_Element):
+    left = 0
+    length = len(arrayBucket)
+    position = -1
+    right = length - 1
 
-logging.basicConfig(level=logging.INFO)
+    # Run loop from 0 to right
+    for left in range(0, right, 1):
+
+        # If search_element is found with
+        # left variable
+        if arrayBucket[left] == search_Element:
+            position = left
+            print("Element found in Array at ", position +
+                  1, " Position with ", left + 1, " Attempt")
+            break
+
+        # If search_element is found with
+        # right variable
+        if arrayBucket[right] == search_Element:
+            position = right
+            print("Element found in Array at ", position + 1,
+                  " Position with ", length - right, " Attempt")
+            break
+        # left += 1
+        right -= 1
+    # If element not found
+    if position == -1:
+        print("Not found in Array with ", left, " Attempt")
 
 
-def search(array, element):
-    total = len(array)
-    mid = int(total / 2)
-    print(array[mid])
-    if array[mid] == element:
-        return mid
-    if array[mid] > element:
-        return search(array[mid:total - 1], element)
-    if array[mid] < element:
-        return search(array[0: mid - 1], element)
-    return None
+# Driver code
+arr = [1, 2, 3, 4, 5]
+search_element = 3
 
+# Function call
+search(arr, search_element)
 
-def main():
-    bucket = [10, 2, 5, 1, 15, 4]
-    element = 5
-
-    position = search(bucket, element)
-    if position:
-        logging.info("Search element %s found at position %s", element, position)
-    else:
-        logging.warning("Search element % not found", element)
-
-
-main()
+# This code is contributed by Dharanendra L V.
