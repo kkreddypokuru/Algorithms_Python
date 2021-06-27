fa6af38eea7d31fbd1134b471fc66ce178c96d99
kkreddypokuru <kkreddypokuru@gmail.com>
2021-06-27 05:40:09 +0530

    search linear algo

---
+++ BinarySearch/binarysearch1.py
@@ -0,0 +1,27 @@
+import random
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def search(items, lt, rt, element):
+    if rt >= lt:
+        mid = (left + right - 1) // 2
+        if element == items[mid]:
+            return mid
+        if element < items[mid]:
+            return search(items, lt, mid - 1, element)
+        else:
+            return search(items, mid + 1, rt, element)
+    else:
+        return -1
+
+
+# elements = random.sample(range(0, 100), 9)
+elements = [1, 2, 3, 4]
+found = 3
+logging.info("elements %s", elements)
+left = 0
+right = len(elements) - 1
+pos = search(elements, left, right, found)
+logging.info("search element found at %s", pos)
