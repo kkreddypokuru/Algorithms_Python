38c2b17367d65a335d8ff3058c12e95c6e5c1649
kkreddypokuru <kkreddypokuru@gmail.com>
2021-10-16 20:20:43 +0530

    merge

--- Sorting/mergeSort.py
+++ Sorting/mergeSort.py
@@ -4,22 +4,37 @@ logging.basicConfig(level=logging.INFO)
 
 
 def splitArray(array):
-    if len(array)>1:
+    if len(array) > 1:
         mid = len(array) // 2
         leftArray = array[:mid]
         rightArray = array[mid:]
-        print("left Array-->", leftArray)
-        splitArray(leftArray,)
-        print("right Array-->", rightArray)
+        print("Split Step:", "array:", array, "-->", "left:", leftArray, "<-->", "right:", rightArray)
+        splitArray(leftArray)
         splitArray(rightArray)
-        mergeArray(leftArray, rightArray)
+        print("merge Step:", "left:", leftArray, "<-->", "right:", rightArray)
+        i = j = k = 0
+        while i < len(leftArray) and j < len(rightArray):
+            if leftArray[i] < rightArray[j]:
+                array[k] = leftArray[i]
+                i += 1
+            else:
+                array[k] = rightArray[j]
+                j += 1
+            k += 1
+        while i < len(leftArray):
+            array[k] = leftArray[i]
+            i += 1
+            k += 1
+        while j < len(rightArray):
+            array[k] = rightArray[j]
+            j += 1
+            k += 1
+        print("array:", array)
+        return array
 
-def mergeArray(l, r):
-    if len(l) == 1 and len(r) ==1:
-        return l+r
-    else:
-        while
 
 a = [5, 3, 2, 1, 6]
-
-splitArray(a)
+# a = [38, 27, 43, 3, 9, 82, 10]
+print(a)
+arr = splitArray(a)
+print('arr---->', arr)
